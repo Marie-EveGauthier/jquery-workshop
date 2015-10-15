3f8a53eaa10a68fb4f5ea92f2c1829d2fca34ace
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2015-10-15 18:19:39 +0000

    fixed a problem about id instead of class

--- script/app.js
+++ script/app.js
@@ -1,12 +1,15 @@
 function iss_position(){
     jQuery.getJSON('http://api.open-notify.org/iss-now.json?callback=?').then(
         function(res) {
+            console.log(res);
+            
             var latitude = res.iss_position.latitude;
             var longitude = res.iss_position.longitude;
     
-            $('img').attr('src','https://maps.googleapis.com/maps/api/staticmap?center=' + latitude +',' + longitude + '&zoom=8&size=400x400&markers=' + latitude +',' + longitude + '&key=AIzaSyB71Ngy5RwV5K_AYfXuWqsbUutbb7B6E_I');
+            $('#iss-position').attr('src','https://maps.googleapis.com/maps/api/staticmap?center=' + latitude +',' + longitude + '&zoom=8&size=400x400&markers=' + latitude +',' + longitude + '&key=AIzaSyB71Ngy5RwV5K_AYfXuWqsbUutbb7B6E_I');
         }
     );
 }
 
-setInterval(iss_position, 5000);
+
+setInterval(iss_position, 4000);
